485d1af8b25cd4feed1063550a24d33601ff3184
Lior Porat <lior@example.com>
2025-10-24 15:16:29 +0300

    Remove share buttom from teacher list

--- src/components/details/teacherDetails/TeacherRow/TeacherRow.tsx
+++ src/components/details/teacherDetails/TeacherRow/TeacherRow.tsx
@@ -29,7 +29,6 @@ const TeacherRow: React.FC<TeacherRowProps> = ({ teacher, handleDeleteTeacher })
             getId={(t) => t.id}
             getInitialValue={(t) => t.name}
             updateExtraFields={() => ({ role: TeacherRoleValues.REGULAR, schoolId: teacher.schoolId })}
-            link={generateSchoolUrl(teacher.schoolId, teacher.id)}
         />
     );
 };
