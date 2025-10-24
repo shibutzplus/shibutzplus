4cd13f28cf4102e4736dd879b64b94372e73ce10
Lior Porat <lior@example.com>
2025-10-24 15:31:12 +0300

    Fix Daily Icon

--- src/components/navigation/HamburgerNav/HamburgerNav.tsx
+++ src/components/navigation/HamburgerNav/HamburgerNav.tsx
@@ -29,7 +29,7 @@ const links: ILink[] = [
     {
         name: routePath.dailySchedule.title,
         p: routePath.dailySchedule.p,
-        Icon: <Icons.calendar size={24} />,
+        Icon: <Icons.dailyCalendar size={24} />,
         withDivider: true,
         withExternal: false,
     },
