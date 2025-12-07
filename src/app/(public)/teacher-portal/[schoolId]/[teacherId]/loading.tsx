9e217b53490c8348e536f8c10700967611733a29
Lior Porat <lior@example.com>
2025-12-07 12:44:39 +0200

    fix loading issues, minor look & feel problems and change color in the preview mode

--- src/app/(public)/teacher-portal/[schoolId]/[teacherId]/loading.tsx
+++ src/app/(public)/teacher-portal/[schoolId]/[teacherId]/loading.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React from "react";
+
+
 import TeacherPortalSkeleton from "@/components/loading/skeleton/TeacherPortalSkeleton/TeacherPortalSkeleton";
 
 const SkeletonPortalSchedule: React.FC = () => {
