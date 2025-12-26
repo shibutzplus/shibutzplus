0e3eba630c1ab779ec1e11774e9e8112232b3afa
Lior Porat <lior@example.com>
2025-12-26 18:13:20 +0200

    cleanup

--- src/hooks/daily/useDailyEditMode.ts
+++ src/hooks/daily/useDailyEditMode.ts
@@ -1,20 +1,12 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 export const useDailyEditMode = () => {
     const [isEditMode, setIsEditMode] = useState(true);
-    const [isLoadingEditPage, setIsLoadingEditPage] = useState(false);
-    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
     const changeDailyMode = () => {
         setIsEditMode((prev) => !prev);
     };
 
-    useEffect(() => {
-        return () => {
-            if (timeoutRef.current) clearTimeout(timeoutRef.current);
-        };
-    }, []);
-
-    return { isEditMode, isLoadingEditPage, changeDailyMode };
+    return { isEditMode, isLoadingEditPage: false, changeDailyMode }; // Keeping isLoadingEditPage false for compatibility
 };
