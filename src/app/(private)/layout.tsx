80b3b03f493e3b8650589c79089586e03b623f2f
Lior Porat <lior@example.com>
2026-01-27 16:17:33 +0200

    refix

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { MainContextProvider } from "@/context/MainContext";
 import AuthGuard from "@/components/auth/AuthGuard";
 
+import Preloader from "@/components/ui/Preloader/Preloader";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <MainContextProvider>
-            <AuthGuard>{children}</AuthGuard>
-        </MainContextProvider>
+        <React.Suspense fallback={<div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}><Preloader /></div>}>
+            <MainContextProvider>
+                <AuthGuard>{children}</AuthGuard>
+            </MainContextProvider>
+        </React.Suspense>
     );
 }
